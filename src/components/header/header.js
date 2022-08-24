7dd001e7db6a2c462f1be88acd7539a14b3f121e
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2022-08-24 07:58:22 +0430

    .

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -58,10 +58,13 @@ export default class Header extends Component{
                                     style={{background:'none',margin:'0 6px',padding:0}}
                                     text={<div className='home-circle'></div>}
                                     options={[
-                                        {text:'خروج از حساب',value:'exit'}
+                                        {text:'ثبت نام',value:'register'},
+                                        {text:'خروج از حساب',value:'exit'},
+
                                     ]}
                                     onChange={(value)=>{
                                         if(value === 'exit'){this.context.logout()}
+                                        if(value === 'register'){this.context.SetState({showRegister:true})}
                                     }}
                                 />
                             ), 

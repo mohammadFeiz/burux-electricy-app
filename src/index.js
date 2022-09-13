104810cfeb57c1c2d082a5976233bf86bd5fd12e
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2022-09-13 17:11:33 +0430

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React,{Component} from 'react';
 import Axios from 'axios';
 import ReactDOM from 'react-dom';
-import { ReactKeycloakProvider,useKeycloak } from "@react-keycloak/web";
-import Keycloak from "keycloak-js";
 import Main from './pages/main';
 import Register from './components/register/register';
 import reportWebVitals from './reportWebVitals';
@@ -356,56 +354,7 @@ class OTPLogin extends Component{
 
 
 
-// export class BuruxAuth extends Component {
-//   render(){
-//     let {goToLanding} = this.props; 
-//     let authClient = new Keycloak({
-//       "realm": "burux",
-//       "url": "https://iam.burux.com/auth/",
-//       "ssl-required": "external",
-//       "resource": "RetailerApp",
-//       "public-client": true,
-//       "verify-token-audience": true,
-//       "use-resource-role-mappings": true,
-//       "confidential-port": 0,
-//       "clientId":'RetailerApp'
-//     })
-//     return (<div><ReactKeycloakProvider authClient={authClient}><Login goToLanding={goToLanding}/></ReactKeycloakProvider></div>);
-//     // return <Main />;
-//   }
-// }
-// function Login(props){
-//   const obj = useKeycloak();
-//   let {authenticated} = obj.keycloak;
-//   if(!authenticated){
-//     let {login} = obj.keycloak; 
-//     try {login()} catch{let a = '';}
-//     return null
-//   }
-//   let {tokenParsed} = obj.keycloak;
-//   let {preferred_username:username,email,groups,name} = tokenParsed;
-//   return (
-//     <InternalLogin
-//       data={{username,email,groups,logout:()=>{
-//         obj.keycloak.logout();
-//         //props.goToLanding()
-//       },groups,name,token:obj.keycloak.token}}
-//     />
-//   )
-// }
-// class InternalLogin extends Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {init:true}
-//   }
-//   logout(){
-//     let {data} = this.props;
-//     data.logout()
-//   }
-//   render(){
-//     return <Main logout={()=>this.logout()}/>
-//   }
-// }
+
 
 ReactDOM.render(
     <OTPLogin />

6d8f48a2b473161fed4f379bc309df15f43b812c
Ali Ahmadi <Ali_Ahmadi_@outlook.com>
2022-11-13 09:33:23 +0330

    kharid-api updated

--- src/apis/kharid-apis.js
+++ src/apis/kharid-apis.js
@@ -181,9 +181,24 @@ export default function kharidApis({getState,token,getDateAndTime,showAlert}) {
         };
       })
       
-      let srcs = await Axios.post(`${baseUrl}/Spree/Products`, { Skus:Skus.toString(), Include: "images" });
-      let included = res.data.data.included;
-      debugger; 
+      let srcs = await Axios.post(`${baseUrl}/Spree/Products`, { Skus:Skus.toString(), Include: "default_variant,images" });
+
+      const {b1Info} = getState();
+      const spreeData = srcs.data.data;
+      const b1Data = b1Info.itemPrices.map((i)=>{
+        const onHand=i.inventory.filter(x=>x.whsCode==="01");
+        return {
+          "itemCode": i.itemCode,
+          "price": 0,
+          "finalPrice": 0,
+          "b1Dscnt": 0,
+          "cmpgnDscnt": 0,
+          "pymntDscnt": 0,
+          "onHand":onHand.length ? onHand[0] : {},
+        };
+      });
+      let resi = this.getMappedAllProducts({ spreeResult: spreeData, b1Result: b1Data, loadType:0 });
+
       return {
         products,
         paymentMethod: result.paymentdetails.paymentTermName,

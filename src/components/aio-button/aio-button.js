a88188f5189576aa43c1a291fcfaa4b1d9904f8e
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2022-11-30 16:44:18 +0330

    Update aio-button.js

--- src/components/aio-button/aio-button.js
+++ src/components/aio-button/aio-button.js
@@ -5,23 +5,23 @@ export default class AIOButtonInterface extends Component{
         let {position} = this.props;
         let props = {rtl:true,...this.props}
         props.animate = false;
-        // if (position === 'bottom'){
-        //     props.popupAttrs = {className:'bottom-popup',style:{bottom:0}};
-        //     props.backColor = 'rgba(0,0,0,0.5)';
-        //     props.popupHeader = (
-        //         <div style={{width:'100%',height:24,display:'flex',justifyContent:'center',alignItems:'center'}}>
-        //             <div style={{width:0,height:0,borderBottom:'6px solid #888',borderLeft:'6px solid transparent',borderRight:'6px solid transparent'}}></div>
-        //         </div>
-        //     )
-        //     props.animate = {bottom:0}
+        if (position === 'bottom'){
+            props.popupAttrs = {className:'bottom-popup',style:{bottom:0}};
+            props.backColor = 'rgba(0,0,0,0.5)';
+            props.popupHeader = (
+                <div style={{width:'100%',height:24,display:'flex',justifyContent:'center',alignItems:'center'}}>
+                    <div style={{width:0,height:0,borderBottom:'6px solid #888',borderLeft:'6px solid transparent',borderRight:'6px solid transparent'}}></div>
+                </div>
+            )
+            props.animate = {bottom:0}
 
-        // }
-        // else if (position === 'top'){
-        //     props.popupAttrs = {className:'top-popup',style:{top:0}};
-        //     props.backColor = 'rgba(0,0,0,0.5)';
-        //     props.animate = {top:0}
+        }
+        else if (position === 'top'){
+            props.popupAttrs = {className:'top-popup',style:{top:0}};
+            props.backColor = 'rgba(0,0,0,0.5)';
+            props.animate = {top:0}
 
-        // }
+        }
         return (
             <AIOButton {...props}/>
         )

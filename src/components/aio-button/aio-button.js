56b96ba70f7312275d0d4d2ddb0794aad51101ed
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2022-11-30 16:24:54 +0330

    Update aio-button.js

--- src/components/aio-button/aio-button.js
+++ src/components/aio-button/aio-button.js
@@ -4,8 +4,9 @@ export default class AIOButtonInterface extends Component{
     render(){
         let {position} = this.props;
         let props = {rtl:true,...this.props}
+        props.animate = false;
         if (position === 'bottom'){
-            props.popupAttrs = {className:'bottom-popup',style:{bottom:-160}};
+            props.popupAttrs = {className:'bottom-popup',style:{bottom:0}};
             props.backColor = 'rgba(0,0,0,0.5)';
             props.popupHeader = (
                 <div style={{width:'100%',height:24,display:'flex',justifyContent:'center',alignItems:'center'}}>
@@ -16,7 +17,7 @@ export default class AIOButtonInterface extends Component{
 
         }
         else if (position === 'top'){
-            props.popupAttrs = {className:'top-popup',style:{top:-160}};
+            props.popupAttrs = {className:'top-popup',style:{top:0}};
             props.backColor = 'rgba(0,0,0,0.5)';
             props.animate = {top:0}
 

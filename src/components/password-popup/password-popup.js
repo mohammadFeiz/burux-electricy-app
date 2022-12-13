91084f240bd5432776844a0da834bd312c5f6c67
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2022-12-13 10:26:54 +0330

    Update password-popup.js

--- src/components/password-popup/password-popup.js
+++ src/components/password-popup/password-popup.js
@@ -3,19 +3,12 @@ import Form from './../../interfaces/aio-form-react/aio-form-react';
 import appContext from "../../app-context";
 export default class PasswordPopup extends Component{
     static contextType = appContext;
-    state = {model:{password:'',passwordConfirm:'',currentPassword:''},changePassword:false}
-    componentDidMount(){
-        let {model} = this.state;
-        let {userInfo} = this.context;
-        model.currentPassword = userInfo.password || '1234567';
-        this.setState({model})
-    }
+    state = {model:{password:'',passwordConfirm:''},changePassword:false}
     async updatePassword(){
         let {updatePassword,rsa_actions} = this.context;
         let {model,changePassword} = this.state;
         let res = await updatePassword(model.password)
         if(res === true){
-            model.currentPassword = model.password;
             model.password = '';
             model.passwordConfirm = '';
             changePassword = false
@@ -45,7 +38,6 @@ export default class PasswordPopup extends Component{
                         validations:[
                             ['required'],
                             ['length>',5],
-                            ['!=',model.currentPassword,{message:'رمز جدید نمی تواند برابر با رمز فعلی باشد'}]
                         ],
                         show:changePassword
                     },
